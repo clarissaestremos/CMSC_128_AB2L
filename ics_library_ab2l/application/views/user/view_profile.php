fcf4a04481aa6a729887746218d69f6db7e6f04f
cjpvillaruel <cjpvillaruel@gmail.com>
2014-03-09 14:35:50 +0800

    updated views for the profile

--- ics_library_ab2l/application/views/user/view_profile.php
+++ ics_library_ab2l/application/views/user/view_profile.php
@@ -39,20 +39,10 @@
                                         if(isset($msg)){
                                             echo $msg;
                                          }
-
+                                    $this->load->view('user/view_upload');
                                  ?>
 
-                                 <form action = "view_upload.php" method="post" enctype="multipart/form-data">
-                                    <div id = "frame" style = "width: 220px;height:220px;border-radius:110px;border:solid 2px;margin-top:40px;margin-left:50px;background:#FFF8DC;"></div><br/><br/><br/>
-                                    <h4 style = "color: #413839; padding-left: 37px; font-size: 22px;font-family:'Lucida Console', Monaco, monospace"><?php echo $name?></h4>
-                                    </div>
-                                    <div id='sib'>
-                                        <ul>
-                                            <li style = "list-style: none;margin-top:-65px"><input type="file" name="file" id="file"></li>
-                                            <li style = "list-style: none;margin-top:-30px;margin-left:250px;"><input type="submit" name="Upload" value="Upload"></li>
-                                        </ul>
-                                    </div>
-                                 </form>
+                                
                                     
                                         
                                 </div>
--- ics_library_ab2l/application/views/user/view_upload.php
+++ ics_library_ab2l/application/views/user/view_upload.php
@@ -13,9 +13,22 @@
  
 <?php echo  form_open_multipart('index.php/user/controller_upload/uploadImage')?>
  
-<input type="file" name="userfile" id = "userfile"/>
+
+
+                                    <div id = "frame" style = "width: 220px;height:220px;border-radius:110px;border:solid 2px;margin-top:40px;margin-left:50px;background:#FFF8DC;"></div><br/><br/><br/>
+                                    <h4 style = "color: #413839; padding-left: 37px; font-size: 22px;font-family:'Lucida Console', Monaco, monospace"><?php echo $name?></h4>
+                                    </div>
+                                    <div id='sib'>
+                                        <ul>
+                                            <li style = "list-style: none;margin-top:-65px"><input type="file" name="userfile" id="file"></li>
+                                            <li style = "list-style: none;margin-top:-30px;margin-left:250px;"><input type="submit" name="Upload" value="Upload"></li>
+                                        </ul>
+                                    </div>
+                               
+
+
  
-<p><input type="submit" name="submit" value="submit" /></p>
+
  
 <?php echo form_close();?>
  
