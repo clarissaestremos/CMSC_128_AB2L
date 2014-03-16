c52c6bbdf3c877a75de09363ef50b5c10d02d68c
Kara Lane Zurbano <karazurbano25@gmail.com>
2014-03-17 04:43:18 +0800

    Documentation

--- ics_library_ab2l/application/controllers/user/controller_books.php
+++ ics_library_ab2l/application/controllers/user/controller_books.php
@@ -1,5 +1,8 @@
-
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+*   This class is used for viewing the list of all books in the library.
+*   You can sort the books by title, subject, author, type, and availability in ascending or descending order.
+*/
 class Controller_books extends CI_Controller {
      function __construct(){
             parent::__construct();
@@ -9,40 +12,56 @@ class Controller_books extends CI_Controller {
             $this->load->library('pagination');
     }
 
+    /**
+    *   Set the title page
+    *   Loads the header, page for viewing all books, and footer
+    */
     function index() {
         $this->load->helper(array('form','html'));
+        
         $data['titlepage'] = "View all books";
         $this->load->view("user/view_header",$data);
         $this->load->view("user/view_all_books");
         $this->load->view("user/view_footer");
     }
 
-   
+    /**
+    *   Gets the data of all the books in the library
+    *   Sorts the books depending on what the user wants
+    *   This function includes the configuration for ajax pagination
+    */
     public function get_book_data1(){
         $this->input->post('serialised_form');
         $sort_by = addslashes($this->input->post('sort_by')); 
         $order_by = addslashes($this->input->post('order_by')); 
+        
+        // getting the number of rows for of a query for computing the total row
         $data['result_all']  = $this->model_get_list->select_all_book_info($sort_by,$order_by,NULL,0,0);
-
         //configuration of the ajax pagination  library.
-        $config['base_url'] = base_url().'index.php/user/controller_books/get_book_data1';        //EDIT THIS BASE_URL IF YOU ARE USING A DIFFERENT URL. 
+        $config['base_url'] = base_url().'index.php/user/controller_books/get_book_data1';
         $config['total_rows'] = count($data['result_all']);
         $config['per_page'] = '10';
         $config['div'] = '#change_here';
         $config['additional_param']  = 'serialize_form1()';
-
         $page=$this->uri->segment(4);       // splits the URI segment by /
         
+        //fetches data from database.
         $data['result'] = $this->model_get_list->select_all_book_info($sort_by,$order_by,$data['result_all'],$config['per_page'],$page);
+        //display data from database
+        
+        //initialize the configuration of the ajax_pagination
         $this->jquery_pagination->initialize($config);
-        //$this->pagination->initialize($config);
+        //create links for pagination
         $data['links'] = $this->jquery_pagination->create_links();
         $this->print_books($data['result'],$data['links']);
        
     }
 
+    /**
+    *   This function produces the table of result
+    */
     public function print_books($result,$links){
-        echo" <table class='body fixed'>
+        echo "<table class='body fixed'>
                 <thead>
                     <tr>
                         <th style='width: 3%;''>#</th>
@@ -56,55 +75,63 @@ class Controller_books extends CI_Controller {
                 </thead>
                                             
                 <tbody>";
-                        $count = 1;
-                            foreach($result as $row){
-                                $this->load->model('model_get_list');
-                                echo "<tr>";
-                                echo "<td>$count</td>";
-                                echo "<td>$row->isbn</td>";
+                    $count = 1;
+                    foreach($result as $row){
+                        echo "<tr>";
+                        echo "<td>$count</td>";
+                        echo "<td>$row->isbn</td>";
 
-                                $data['multi_valued'] = $this->model_get_list->get_book_subjects($row->id);
-                                $subject="";
-                                foreach($data['multi_valued'] as $subject_list){
-                                $subject = $subject."{$subject_list->subject}<br/>";
-                                }
-                                echo "<td>".$subject."</td>";
+                        $data['multi_valued'] = $this->model_get_list->get_book_subjects($row->id);
+                        $subject="";
+                        foreach($data['multi_valued'] as $subject_list){
+                            $subject = $subject."{$subject_list->subject}<br/>";
+                        }
+                        echo "<td>".$subject."</td>";
 
-                                echo "<td><b>$row->title</b> <br/>";
-                                $data['multi_valued'] = $this->model_get_list->get_book_authors($row->id);
-                                $authors="";
-                                foreach($data['multi_valued'] as $authors_list){
-                                    $authors = $authors."{$authors_list->author},";
-                                }
-                                echo "$authors ($row->year_of_pub)</td>";
+                        echo "<td><b>$row->title</b> <br/>";
+                        $data['multi_valued'] = $this->model_get_list->get_book_authors($row->id);
+                        $authors="";
+                        foreach($data['multi_valued'] as $authors_list){
+                            $authors = $authors."{$authors_list->author},";
+                        }
+                        echo "$authors ($row->year_of_pub)</td>";
 
-                                if ($row->type == "BOOK"){
-                                    echo "<td><center><img title = 'BOOK' width = 30px height = 30px src='../../images/type_book.png'/></center></td>";
-                                }
-                                else
-                                    //image source: http://www.webweaver.nu/clipart/img/education/diploma.png
-                                    echo "<td><img title = 'THESIS/SP' width = 30px height = 30px src='../../images/type_thesis.png' /></td>";
+                        if ($row->type == "BOOK"){
+                            echo "<td><center><img title = 'BOOK' width = 30px height = 30px src='../../images/type_book.png'/></center></td>";
+                        }
+                        else{
+                            //image source: http://www.webweaver.nu/clipart/img/education/diploma.png
+                            echo "<td><img title = 'THESIS/SP' width = 30px height = 30px src='../../images/type_thesis.png' /></td>";
+                        }
+                            
+                        echo "<td>".$row->no_of_available. "/" . $row->quantity."</td>";
 
-                                echo "<td>".$row->no_of_available. "/" . $row->quantity."</td>";
-                                if($row->no_of_available != 0)
-                                    echo "<td><form method='POST' action='controller_reserve_book/verify_login/$row->id'>
-                                            <input type='submit' class='background-red table-button' value='Reserve Book'>                                                       </form>
-                                        </td>";
-                                else
-                                    echo "<td><form method='POST' action='controller_reserve_book/verify_login/$row->id'>
-                                            <input type='submit' class='background-red table-button' value='Waitlist'></form></td>";
-                                
-                                echo "</tr>";
-                        $count++;
+                        if ($row->type == "BOOK"){
+                            if($row->no_of_available != 0){
+                                echo "<td><form method='POST' action='controller_reserve_book/verify_login/$row->id'>
+                                        <input type='submit' class='background-red table-button' value='Reserve Book'>
+                                        </form>
+                                    </td>";
                             }
-            echo" </tbody>
-        </table><div class='pagination'>";
-                                           echo $links;
-                                        echo "</div>";
+                            else{
+                                echo "<td><form method='POST' action='controller_reserve_book/verify_login/$row->id'>
+                                        <input type='submit' class='background-red table-button' value='Waitlist'></form>
+                                    </td>";
+                            }
+                        }
+                        else{
+                            echo "<td>Visit admin to borrow SP/Thesis</td>";
+                        }
+                        echo "</tr>";
+                        
+                        $count++;
+                    }
+                echo" </tbody>
+            </table><div class='pagination'>";
+            echo $links;
+            echo "</div>";
     }
 }
-
-
 
 /* End of file home_controller.php */
 /* Location: ./application/controllers/user/controller_home.php */
--- ics_library_ab2l/application/controllers/user/controller_search_book.php
+++ ics_library_ab2l/application/controllers/user/controller_search_book.php
@@ -1,18 +1,25 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+*	This class is used for searching to know if a book is available in the library.
+*/
 class Controller_search_book extends CI_Controller {
  	function __construct(){
 		parent::__construct();
 		$this->load->model('model_search_book');
-
 		$this->load->model('model_get_list');
-		$this->load->model('model_reserve_book');
 		$this->load->library('Jquery_pagination');
 		$this->load->library('pagination');
 	}
 
-    function index() {
+	/**
+	*	Gets the input if the user used the header search for searching
+	*	Set the title page
+	*	Loads the header, page for searching a book, and footer
+	*/
+    function index(){
     	if(isSet($_POST['category'])) $data['category']=addslashes($this->input->post('category'));
     	if(isSet($_POST['hinput'])) $data['text']=addslashes($this->input->post('hinput'));
+    	
     	$data['titlepage'] = "Books - Search";
 
         $this->load->helper(array('form','html'));
@@ -21,9 +28,11 @@ class Controller_search_book extends CI_Controller {
         $this->load->view("user/view_footer");
     }
 
-    //AJAX. DO NOT TOUCH IF YOU ARE NOT FAMILIAR WITH IT. Nah.
-	//autosuggest function. Fetches data from server and loads it using AJAX.
-	public function autosuggest(){
+    /**
+    *	Prints suggestions for the user.
+    *	Triggered everytime a user type a character in the search input field
+    */
+    public function autosuggest(){
 		//for escape characters
 		$str = addslashes($this->input->post('str'));
 		$category = addslashes($this->input->post('category'));
@@ -35,13 +44,19 @@ class Controller_search_book extends CI_Controller {
 		}
 		echo "</ul></div>";
 	}
-
+/**
 	public function refreshpage(){
 		redirect('index.php/user/controller_search_book', 'refresh');
 	}
+*/
 
+	/**
+    *   Gets the data of the book included in the results in searching
+    *   This function includes the configuration for ajax pagination
+    */
 	public function get_book_data(){
 		$this->input->post('serialised_form');
+		
 		//input declaration using POST in form
 		$data['str'] = addslashes($this->input->post('sinput'));
 		$data['category'] = addslashes($this->input->post('category'));
@@ -51,7 +66,6 @@ class Controller_search_book extends CI_Controller {
 		if($this->input->post('subject')!=="") $data['subject'] = addslashes($this->input->post('subject'));
 		if($this->input->post('tag_name')!=="") $data['tag_name'] = addslashes($this->input->post('tag_name'));
 		
-
 		// getting the number of rows for of a query for computing the total row
 		$row_number=$this->model_search_book->fetch_book_data($data,0,0);
 		//configuration of the ajax pagination  library.
@@ -61,98 +75,104 @@ class Controller_search_book extends CI_Controller {
 		$config['div'] = '#list_area';
 		$config['additional_param']  = 'serialize_form()';
 		$page=$this->uri->segment(4);		// splits the URI segment by /
+		
 		//fetches data from database.
 		$row = $this->model_search_book->fetch_book_data($data,$config['per_page'],$page);
 		//display data from database
 		
-		
 		//initialize the configuration of the ajax_pagination
 		$this->jquery_pagination->initialize($config);
-		//$this->pagination->initialize($config);
 		//create links for pagination
 		$data['links'] = $this->jquery_pagination->create_links();
         $this->print_books($row, $data['links']);
-		//echo $this->pagination->create_links();
 
 	}
 
+	/**
+    *   This function produces the table of result
+    */
 	public function print_books($row, $links){
-		//display data from database
 		if($row->num_rows()>0){
 			echo"<div class='panel datasheet'>
-                <div class='header text-center background-red'>
-                    Search Results
-                </div>
-                <table class='body fixed'>
-                <thead>
-                    <tr>
-                        <th style='width: 3%;''>#</th>
-                        <th style='width: 15%;' nowrap='nowrap'>ISBN</th>
-                        <th style='width: 15%;' nowrap='nowrap'>Subject</th>
-                        <th style='width: 50%;' nowrap='nowrap'>Material</th>
-                        <th style='width: 5%;' nowrap='nowrap'>Type</th>
-                        <th style='width: 8%;' nowrap='nowrap'>Availability</th>
-                        <th style='width: 13%;' nowrap='nowrap'>Action</th>
-                    </tr>
-                </thead>
+	                <div class='header text-center background-red'>
+	                    Search Results
+	                </div>
+	                <table class='body fixed'>
+		                <thead>
+		                    <tr>
+		                        <th style='width: 3%;''>#</th>
+		                        <th style='width: 15%;' nowrap='nowrap'>ISBN</th>
+		                        <th style='width: 15%;' nowrap='nowrap'>Subject</th>
+		                        <th style='width: 50%;' nowrap='nowrap'>Material</th>
+		                        <th style='width: 5%;' nowrap='nowrap'>Type</th>
+		                        <th style='width: 8%;' nowrap='nowrap'>Availability</th>
+		                        <th style='width: 13%;' nowrap='nowrap'>Action</th>
+		                    </tr>
+		                </thead>
                                             
-                <tbody>";
-	            	$count=1;
-			        foreach($row->result() as $row){
-						$this->load->model('model_get_list');
-						echo "<tr>
-								<td>$count</td>
-								<td>$row->isbn</td>";
-						
-						$data['multi_valued'] = $this->model_get_list->get_book_subjects($row->id);
-		                $subject="";
-		                foreach($data['multi_valued'] as $subject_list){
-		                $subject = $subject."{$subject_list->subject}<br/>";
-		                }
-		                echo "<td>".$subject."</td>";
+		                <tbody>";
+			            	$count = 1;
+		                    foreach($row->result() as $row){
+		                        $this->load->model('model_get_list');
+		                        echo "<tr>";
+		                        echo "<td>$count</td>";
+		                        echo "<td>$row->isbn</td>";
 
-		                
-				                            echo "<td><b>$row->title</b> <br/>";
-				                            $data['multi_valued'] = $this->model_get_list->get_book_authors($row->id);
-				                            $authors="";
-				                            foreach($data['multi_valued'] as $authors_list){
-				                                $authors = $authors."{$authors_list->author},";
-				                            }
-				                            echo "$authors ($row->year_of_pub)</td>";
+		                        $data['multi_valued'] = $this->model_get_list->get_book_subjects($row->id);
+		                        $subject="";
+		                        foreach($data['multi_valued'] as $subject_list){
+		                            $subject = $subject."{$subject_list->subject}<br/>";
+		                        }
+		                        echo "<td>".$subject."</td>";
 
-				                            if ($row->type == "BOOK"){
-				                                echo "<td><center><img title = 'BOOK' width = 30px height = 30px src='../../images/type_book.png'/></center></td>";
-				                            }
-				                            else
-				                                //image source: http://www.webweaver.nu/clipart/img/education/diploma.png
-				                                echo "<td><img title = 'THESIS/SP' width = 30px height = 30px src='../../images/type_thesis.png' /></td>";
+		                        echo "<td><b>$row->title</b> <br/>";
+		                        $data['multi_valued'] = $this->model_get_list->get_book_authors($row->id);
+		                        $authors="";
+		                        foreach($data['multi_valued'] as $authors_list){
+		                            $authors = $authors."{$authors_list->author},";
+		                        }
+		                        echo "$authors ($row->year_of_pub)</td>";
 
-				                            echo "<td>".$row->no_of_available. "/" . $row->quantity."</td>";
+		                        if ($row->type == "BOOK"){
+		                            echo "<td><center><img title = 'BOOK' width = 30px height = 30px src='../../images/type_book.png'/></center></td>";
+		                        }
+		                        else{
+		                            //image source: http://www.webweaver.nu/clipart/img/education/diploma.png
+		                            echo "<td><img title = 'THESIS/SP' width = 30px height = 30px src='../../images/type_thesis.png' /></td>";
+		                        }
+		                            
+		                        echo "<td>".$row->no_of_available. "/" . $row->quantity."</td>";
 
-											if($row->no_of_available > 0)
-				                               echo "<td><form method='POST' action='controller_reserve_book/verify_login/$row->id'>
-				                                        <input type='submit' class='background-red table-button' value='Reserve Book'>                                                       </form>
-				                                    </td>";
-				                            else
-				                                echo "<td><form method='POST' action='controller_reserve_book/verify_login/$row->id'>
-				                                        <input type='submit' class='background-red table-button' value='Waitlist'></form></td>";
-				                            
-				                            echo "</tr>";
-				                    $count++;
-				                        }
-				        echo" </tbody>
-				    </table><div class='pagination'>";
-				    echo $links;
-
-	                "</div></div>";
+		                        if ($row->type == "BOOK"){
+		                            if($row->no_of_available != 0){
+		                                echo "<td><form method='POST' action='controller_reserve_book/verify_login/$row->id'>
+		                                        <input type='submit' class='background-red table-button' value='Reserve Book'></form>
+		                                    </td>";
+		                            }
+		                            else{
+		                                echo "<td><form method='POST' action='controller_reserve_book/verify_login/$row->id'>
+		                                        <input type='submit' class='background-red table-button' value='Waitlist'></form>
+		                                    </td>";
+		                            }
+		                        }
+		                        else{
+		                            echo "<td>Visit admin to borrow SP/Thesis</td>";
+		                        }
+		                        echo "</tr>";
+		                        
+		                        $count++;
+		                    }
+		                echo" </tbody>
+		            </table><div class='pagination'>";
+		            echo $links;
+		            echo "</div>";
 		}
-		
 		else if($row->num_rows() == 0){
 			echo"<div class='panel datasheet'>
-                <div class='header text-center background-red'>
-                    No results found.
-                </div></div>";
-
+            		<div class='header text-center background-red'>
+                    	No results found.
+                	</div>
+                </div>";
 		}
 
 		
