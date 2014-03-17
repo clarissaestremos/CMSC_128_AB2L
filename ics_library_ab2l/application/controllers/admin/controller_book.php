fff569e2e2cd4e0ed5498818c9198c40963baecd
Kara Lane Zurbano <karazurbano25@gmail.com>
2014-03-17 13:52:18 +0800

    Optimize and document admin/controller_book (unfinished in edit book part)

--- ics_library_ab2l/application/controllers/admin/controller_book.php
+++ ics_library_ab2l/application/controllers/admin/controller_book.php
@@ -1,147 +1,164 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
 include_once("controller_log.php");
+/**
+*   This class is used for viewing/editing the list of all books in the library.
+*   You can sort the books by title, subject, author, type, and availability in ascending or descending order.
+*/
 class Controller_book extends Controller_log {   
- function __construct(){
-            parent::__construct();
-            $this->load->helper("url");
-            $this->load->library('Jquery_pagination');
-            $this->load->model('model_get_list');
-            $this->load->model('model_book');
-            $this->load->library('pagination');
+	function __construct(){
+        parent::__construct();
+        $this->load->helper("url");
+        $this->load->library('Jquery_pagination');
+        $this->load->model('model_get_list');
+        $this->load->model('model_book');
+        $this->load->library('pagination');
     }
 
+    /**
+    *   Set the title page
+    *   Loads the header, side container, page for viewing all books, and footer
+    */
 	public function index()
 	{
-		//$session_user = $this->checklogin();
-		$this->load->model('model_book');
-		$data['query'] = $this->model_book->select_all_book_info();
 		$data['parent'] = "Books";
     	$data['current'] = "View Books";
-    	$data['user'] = $this->session->userdata('logged_in');
-
+    	
     	$this->load->helper(array('form','html'));
 	    $this->load->view("admin/view_header",$data);
 	    $this->load->view("admin/view_aside");
-	    $this->load->view('admin/view_books');
+	    $this->load->view("admin/view_books");
 	    $this->load->view("admin/view_footer");
 		
 	}
 
+	/**
+    *   Gets the data of all the books in the library
+    *   Sorts the books depending on what the admin wants
+    *   This function includes the configuration for ajax pagination
+    */
 	public function get_book_data1(){
         $this->input->post('serialised_form');
         $sort_by = addslashes($this->input->post('sort_by')); 
         $order_by = addslashes($this->input->post('order_by')); 
+        
+        // getting the number of rows for of a query for computing the total row
         $data['result_all']  = $this->model_get_list->select_all_book_info($sort_by,$order_by,NULL,0,0);
-
         //configuration of the ajax pagination  library.
-        $config['base_url'] = base_url().'index.php/admin/controller_book/get_book_data1';        //EDIT THIS BASE_URL IF YOU ARE USING A DIFFERENT URL. 
+        $config['base_url'] = base_url().'index.php/user/controller_books/get_book_data1';
         $config['total_rows'] = count($data['result_all']);
         $config['per_page'] = '10';
         $config['div'] = '#change_here';
         $config['additional_param']  = 'serialize_form1()';
-
         $page=$this->uri->segment(4);       // splits the URI segment by /
         
+        //fetches data from database.
         $data['result'] = $this->model_get_list->select_all_book_info($sort_by,$order_by,$data['result_all'],$config['per_page'],$page);
+        //display data from database
+        
+        //initialize the configuration of the ajax_pagination
         $this->jquery_pagination->initialize($config);
-        //$this->pagination->initialize($config);
+        //create links for pagination
         $data['links'] = $this->jquery_pagination->create_links();
         $this->print_books($data['result'],$data['links']);
        
     }
 
+    /**
+    *   This function produces the table of result
+    */
     public function print_books($result, $links){
 		echo" <table class='body'>
                 <thead>
                     <tr>
-                        <th style'width: 5%;'>#</th>
-                        				<th style='width: 10%;'><center>Call Number</center></th>
-                                        <th style='width: 10%;'><center>Subject</center></th>
-                                        <th style='width: 50%;'><center>Material</center></th>
-                                        <th style='width: 7%;'><center>Type</center></th>
-                                        <th style='width: 5%;'><center>Qty</center></th>
-                                        <th style='width: 8%;'></th>
-                                        <th style='width: 8%;'></th>
-                                    </tr>
-                                </thead> 
-                                <tbody>";
-                                    
-                                        $count = 1;
-                                        foreach($result as $row){
-                                            echo "<tr>";
-                                            echo "<td>$count</td>";
-                                            $data['query1'] = $this->model_book->get_book_call_numbers($row->id);
-                                            $call_number="";
-                                            foreach($data['query1'] as $call_number_list){
-                                                $call_number .= "{$call_number_list->call_number}<br/> ";
-                                            }
-                                            echo "<td>{$call_number}</td>"; 
-                                            $data['query1'] = $this->model_book->get_book_subjects($row->id);
-                                            $subjects ="";
-                                            foreach($data['query1'] as $subjects_list){
-                                                $subjects .= "{$subjects_list->subject}<br/> ";
-                                            }
-                                            echo "<td>{$subjects}</td>";
-                                            echo "<td><b>{$row->title}</b><br/>";
-                                            $data['query1'] = $this->model_book->get_book_authors($row->id);
-                                            $authors ="";
-                                            foreach($data['query1'] as $authors_list){
-                                                $authors .= "{$authors_list->author},";
-                                            }
-                                           	echo"{$authors} ({$row->year_of_pub})<br/>";
-                                           		if ($row->isbn != NULL)
-                                           			echo "ISBN:{$row->isbn}";
-                                           echo"</td>";
-                                           
-                                            //image source: http://3.bp.blogspot.com/-hUGEJQbn1Hk/ULY_bdWVgdI/AAAAAAAAAd0/Z2vFFfsae_4/s1600/Red_book_cover.png
-			                                if ($row->type == "BOOK"){
-			                                    echo "<td><center><img title = 'BOOK' width = 30px height = 30px src='../../images/type_book.png'/></center></td>";
-			                                }
+                        <th style='width: 5%;'>#</th>
+        				<th style='width: 10%;'><center>Call Number</center></th>
+                        <th style='width: 10%;'><center>Subject</center></th>
+                        <th style='width: 50%;'><center>Material</center></th>
+                        <th style='width: 7%;'><center>Type</center></th>
+                        <th style='width: 5%;'><center>Qty</center></th>
+                        <th style='width: 8%;'></th>
+                        <th style='width: 8%;'></th>
+                    </tr>
+                </thead> 
+                <tbody>";                
+                    $count = 1;
+                    foreach($result as $row){
+                        echo "<tr>";
+                        echo "<td>$count</td>";
 
-			                                else
-                                           		 //image source: http://www.webweaver.nu/clipart/img/education/diploma.png
-			                                	 echo "<td><img title = 'THESIS/SP' width = 30px height = 30px src='../../images/type_thesis.png' /></td>";
+                        $data['query1'] = $this->model_get_list->get_book_call_numbers($row->id);
+                        $call_number="";
+                        foreach($data['query1'] as $call_number_list){
+                            $call_number .= "{$call_number_list->call_number}<br/> ";
+                        }
+                        echo "<td>{$call_number}</td>";
+                        
+                        $data['query1'] = $this->model_get_list->get_book_subjects($row->id);
+                        $subjects ="";
+                        foreach($data['query1'] as $subjects_list){
+                            $subjects .= "{$subjects_list->subject}<br/> ";
+                        }
+                        echo "<td>{$subjects}</td>";
 
+                        echo "<td><b>{$row->title}</b><br/>";
+                        $data['query1'] = $this->model_get_list->get_book_authors($row->id);
+                        $authors ="";
+                        foreach($data['query1'] as $authors_list){
+                            $authors .= "{$authors_list->author},";
+                        }
+                       	echo"{$authors} ({$row->year_of_pub})<br/>";
+                       		if ($row->isbn != NULL)
+                       			echo "ISBN:{$row->isbn}";
+                       echo"</td>";
+                       
+                        //image source: http://3.bp.blogspot.com/-hUGEJQbn1Hk/ULY_bdWVgdI/AAAAAAAAAd0/Z2vFFfsae_4/s1600/Red_book_cover.png
+                        if ($row->type == "BOOK"){
+                            echo "<td><center><img title = 'BOOK' width = 30px height = 30px src='../../images/type_book.png'/></center></td>";
+                        }
 
-                                            echo "<td>{$row->no_of_available}/{$row->quantity}</td>
-
-                                            <td>";
-                                            $base = base_url();
-                                                echo "<form action='$base"."index.php/admin/controller_book/edit/' method='post'>
-                                                    <input type=\"hidden\" name=\"id\" value=\"{$row->id}\" />
-                                                    <input type='submit' class='background-red' name='edit' value='Edit' enabled/>
-                                                </form>
-                                                </td>
-                                                <td>
-                                                <form action='$base"."index.php/admin/controller_book/delete/' id='mydeleteform$count' method='post'>
-                                                    <input type=\"hidden\"  name=\"id\" value=\"{$row->id}\" />
-                                                    <input type='submit' name='delete' class='background-red' value='Delete' onclick='return trySubmit(mydeleteform$count);' enabled/>
-                                                </form>
-                                                </td>
-                                                </tr>";
+                        else
+                       		 //image source: http://www.webweaver.nu/clipart/img/education/diploma.png
+                        	 echo "<td><img title = 'THESIS/SP' width = 30px height = 30px src='../../images/type_thesis.png' /></td>";
 
 
-                                            echo "</tr>";
-                                            $count++;
-                                        }
-                                   
-                              echo"  </tbody>
-                            </table>
-                            <div class='footer pagination'>";
-                                echo $links;
-                            echo"</div>";
+                        echo "<td>{$row->no_of_available}/{$row->quantity}</td>
+
+                        <td>";
+                        $base = base_url();
+                        echo "<form action='$base"."index.php/admin/controller_book/edit/' method='post'>
+                                <input type=\"hidden\" name=\"id\" value=\"{$row->id}\" />
+                                <input type='submit' class='background-red' name='edit' value='Edit' enabled/>
+                            </form>
+                        </td>
+                        <td>
+                            <form action='$base"."index.php/admin/controller_book/delete/' id='mydeleteform$count' method='post'>
+                                <input type=\"hidden\"  name=\"id\" value=\"{$row->id}\" />
+                                <input type='submit' name='delete' class='background-red' value='Delete' onclick='return trySubmit(mydeleteform$count);' enabled/>
+                            </form>
+                        </td>";
+                        echo "</tr></tr>";
+                        $count++;
+                    }
+               
+          	echo "</tbody>
+        	</table>
+	        <div class='footer pagination'>";
+	            echo $links;
+	        echo"</div>";
 
     }
-	/*ADD BOOK FUNCTIONS*/
-	function add_book(){
-		$this->load->view("view_add_book");
-	}
 	
+	/**
+	*	Adds a book in the database
+	*/
 	public function call_add(){
-			$session_user = $this->session->userdata('logged_in')['username'];
-			if($this->session->userdata('logged_in_type')!="admin")
+		//gets the username of the account currently logged in
+		$session_user = $this->session->userdata('logged_in')['username'];
+
+		//checks if the user is an administrator
+		if($this->session->userdata('logged_in_type')!="admin")
             redirect('index.php/user/controller_login', 'refresh');
+		
 		if(isset($_POST['sub'])){
 			$call_number = array_unique ($_POST['call_number']);
 			$title = htmlspecialchars($_POST['title1']);
@@ -155,7 +172,6 @@ class Controller_book extends Controller_log {
 			$book_stat = 0;
 			$tags = array_unique ($_POST['tags']);
 			
-			$this->load->model("model_book");
 			$id = $this->model_book->insert_book_info($call_number, $title, $year_of_pub, $isbn, $type, $no_of_available, $quantity, $book_stat, $author, $subject, $tags);
 			if($type== "BOOK"){
 				$type = ucfirst($type);
@@ -172,78 +188,93 @@ class Controller_book extends Controller_log {
 		}
 	}
 	
+	/**
+	*	Checks if the isbn is already registered in the database
+	*/
 	public function check_isbn($isbn){
-
-		 $this->db->where('isbn',$isbn);
-            $query = $this->db->get('book')->num_rows();
-            if($query == 0 ){
-                  
-                       echo 'userOk';
-                       return true;
-                     
-              }
-            else{ echo 'userNo';
-            	return false;
-            }
+		$this->db->where('isbn',$isbn);
+        $query = $this->db->get('book')->num_rows();
+        if($query == 0 ){
+           echo 'userOk';
+           return true;
+	    }
+	    else{
+	    	echo 'userNo';
+	    	return false;
+	    }
 	}
 
+	/**
+	*	Prompts that the admin successfully added a new material
+	*/
 	public function call_success(){
-			if($this->session->userdata('logged_in_type')!="admin")
-            redirect('index.php/user/controller_login', 'refresh');
-			$base = base_url();
-			echo "<div id='mysuccess' title='Add Book Success'>
-			<h6>You have successfully added a new material!!</h6>
-			</div>
-			<script src='$base/js/jquery-1.10.2.min.js'></script>
-			<script src='$base/js/jquery-ui.js'></script>
-			<link rel='stylesheet' href='$base/style/jquery-ui.css'/>
-			<script>
-					$('#mysuccess').dialog({
-			            modal: true,
-			            closeOnEscape: true,
-			            resizable: false,
-					      width: 300,
-					      minHeight: 200,
-			            closeText: 'show',
-			            show: {
-			              effect: 'fadeIn',
-			              duration: 200
-			            },
-			            draggable: false,
-			            close: function(event, ui){
-			                window.location.replace('$base/index.php/admin/controller_add_books');
-			            },
-			            buttons : {
-			              'Ok': function() {
-			                  window.location.replace('$base/index.php/admin/controller_add_books');
-			              },
-			            }
-			 
-			        });
-			</script>";
+
+		//checks if the user is an administrator
+		if($this->session->userdata('logged_in_type')!="admin")
+        	redirect('index.php/user/controller_login', 'refresh');
+		$base = base_url();
+		echo "<div id='mysuccess' title='Add Book Success'>
+		<h6>You have successfully added a new material!!</h6>
+		</div>
+		<script src='$base/js/jquery-1.10.2.min.js'></script>
+		<script src='$base/js/jquery-ui.js'></script>
+		<link rel='stylesheet' href='$base/style/jquery-ui.css'/>
+		<script>
+			$('#mysuccess').dialog({
+	            modal: true,
+	            closeOnEscape: true,
+	            resizable: false,
+			      width: 300,
+			      minHeight: 200,
+	            closeText: 'show',
+	            show: {
+	              effect: 'fadeIn',
+	              duration: 200
+	            },
+	            draggable: false,
+	            close: function(event, ui){
+	                window.location.replace('$base/index.php/admin/controller_add_books');
+	            },
+	            buttons : {
+	              'Ok': function() {
+	                  window.location.replace('$base/index.php/admin/controller_add_books');
+	              },
+	            }
+	 
+	        });
+		</script>";
 	}
 	
-	/*UPDATE book*/
-	//EDIT
+	/**
+	*	function for editing an attribute in a material
+	*/
 	function edit(){
+		//gets the username of the account currently logged in
 		$session_user = $this->session->userdata('logged_in')['username'];
+
+		//checks if the user is an administrator
 		if($this->session->userdata('logged_in_type')!="admin")
             redirect('index.php/user/controller_login', 'refresh');
-		$id = $_POST['id'];
-		$this->load->model('model_book');
-		$book = $this->model_book->get_by_id($id);
-		$data['book'] = $book;
+		
+		if(isset($_POST['id'])){
+			$id = $_POST['id'];
+			$book = $this->model_get_list->get_by_id($id);
+			$data['book'] = $book;
 
-		$data['query'] = $this->model_book->select_all_book_info();
-		$data['parent'] = "Books";
-    	$data['current'] = " Books";
-    	$data['user'] = $this->session->userdata('logged_in');
+			$data['parent'] = "Books";
+	    	$data['current'] = "Books";
+	    	$data['user'] = $this->session->userdata('logged_in');
 
-		$this->load->helper(array('form','html'));
-		$this->load->view("admin/view_header",$data);
-		$this->load->view("admin/view_aside");
-		$this->load->view('admin/view_edit_book', $data);
-		$this->load->view("admin/view_footer");
+	    	//loads the header, side container, page for editing a material, and footer
+			$this->load->helper(array('form','html'));
+			$this->load->view("admin/view_header",$data);
+			$this->load->view("admin/view_aside");
+			$this->load->view('admin/view_edit_book', $data);
+			$this->load->view("admin/view_footer");	
+		}
+		else{
+			redirect('index.php/admin/controller_book', 'refresh');
+		}
 	}
 
 	function edit_book(){
