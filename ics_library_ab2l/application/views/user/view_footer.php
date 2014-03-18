6269fe207abca11c65ea1ee94de62ecd5482daa5
Maraiah <maraiahgene1123@gmail.com>
2014-03-18 17:38:31 +0800

    tabbing

--- ics_library_ab2l/application/views/user/view_footer.php
+++ ics_library_ab2l/application/views/user/view_footer.php
@@ -6,10 +6,10 @@
 </style>
 <div class="site-footer" style="box-shadow: 0px -1px 2px #000; border-top:2px solid #9d212d;">
 		<div class="site-center" >
-					<div class="center width-fill" >
-						<div class="col" >
-	                    	<div class="cell" >
-		                        <ul  class="nav col width-fill" id="hovfooter" style="font-size:12px;">
+				<div class="center width-fill" >
+					<div class="col" >
+	                    <div class="cell" >
+			                	<ul  class="nav col width-fill" id="hovfooter" style="font-size:12px;">
 		                            <img src="<?php echo base_url();?>/images/icslogo.png"/ style="width:45px;height:45px;"><li class="float-left"><a class='color-blue' href="<?php echo base_url(); ?>">ICS Library System</a></li>
 		                            <li class="float-left"><a class='color-blue' href="<?php echo base_url(); ?>index.php/user/controller_about">About</a></li>
 		                            <li class="float-left"><a class='color-blue' href="<?php echo base_url(); ?>index.php/user/controller_search_book">Search</a></li>
@@ -17,9 +17,9 @@
 		                            <li class="float-left"><a  class='color-blue' href="<?php echo base_url(); ?>index.php/user/controller_contact">Contact</a></li>
 		                            <li class="float-left"><a class='color-blue' href="<?php echo base_url(); ?>/index.php/user/controller_usermanual" target="_blank">User Manual</a></li>
 		                        </ul>
-	                   		 </div>
+	                   		</div>
                 		</div>
-           			 </div>
+           			</div>
 				</div>
 			</div>
 		</div>
