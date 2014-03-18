6b35a9b1b4135ceb25be28673f06d01baacc15a4
Klarence Kim <kimanangan@gmail.com>
2014-03-18 18:05:04 +0800

    Update model_stat.php

--- ics_library_ab2l/application/models/model_stat.php
+++ ics_library_ab2l/application/models/model_stat.php
@@ -1,6 +1,8 @@
 <?php
 	class Model_stat extends CI_Model{
-		public function get_stat(){
+	
+		public function get_stat()
+		{
 			$sqlQuery = "(SELECT * FROM book ORDER BY book_stat DESC LIMIT 10) ORDER BY book_stat DESC;";
 			$query = $this->db->query($sqlQuery);
 			
