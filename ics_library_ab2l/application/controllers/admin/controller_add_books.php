20a7c58530a46c29e81e773f630cf7560709914a
arriane1111 <krystelarrianecortez@gmail.com>
2014-03-18 18:08:57 +0800

    documentation for controller_add_books.php

--- ics_library_ab2l/application/controllers/admin/controller_add_books.php
+++ ics_library_ab2l/application/controllers/admin/controller_add_books.php
@@ -4,18 +4,21 @@ class Controller_add_books extends CI_Controller {
     function index() {
     	$this->add_book(null);
     }
-    function add_book($msg){
-    	$data['parent'] = "Books";
-    	$data['current'] = "Add Books";
-    	if($msg != null)
-    		$data['message'] = "<span class='color-green'>You have successfully added the book: $msg</span>";
-
-    	$this->load->helper(array('form','html'));
-	    $this->load->view("admin/view_header",$data);
-	    $this->load->view("admin/view_aside");
-	    $this->load->view("admin/view_add_books");
-	    $this->load->view("admin/view_footer");
-    }
+    /*
+    * Calls the view_add_book, which has the form for adding new books
+    */
+	function add_book($msg){
+	$data['parent'] = "Books";
+	$data['current'] = "Add Books";
+	if($msg != null)
+	    $data['message'] = "<span class='color-green'>You have successfully added the book: $msg</span>";
+	
+	$this->load->helper(array('form','html'));
+	$this->load->view("admin/view_header",$data);
+	$this->load->view("admin/view_aside");
+	$this->load->view("admin/view_add_books");
+	$this->load->view("admin/view_footer");
+	}
 }
-/* End of file home_controller.php */
-/* Location: ./application/controllers/user/controller_home.php */
+/* End of file controller_add_books.php */
+/* Location: ./application/controllers/admin/controller_add_books.php */
