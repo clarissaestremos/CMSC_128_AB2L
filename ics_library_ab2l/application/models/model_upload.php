bf50b5b4d359582a0d633f48c24543476e979d7d
Klarence Kim <kimanangan@gmail.com>
2014-03-18 18:07:14 +0800

    Update model_upload.php

--- ics_library_ab2l/application/models/model_upload.php
+++ ics_library_ab2l/application/models/model_upload.php
@@ -3,22 +3,23 @@
 
 		public function __construct()
 		{
-	    	$this->load->database();
+	    		$this->load->database();
 		}
 	
 		public function add_picture()
 		{    
-	    	$data=array(
-	    		'account_no'=>$this->input->post('account_no'),
-	    		'file_name'=>$this->input->post('userfile'),
-	    		);
-	
-	    	$this->db->insert('profile_pictures',$data);
+		    	$data=array(
+		    		'account_no'=>$this->input->post('account_no'),
+		    		'file_name'=>$this->input->post('userfile'),
+		    		);
+		
+		    	$this->db->insert('profile_pictures',$data);
 	    }
 	  
-		function get_filename($id){
-	    	$query = $this->db->get_where('profile_pictures', array('account_no' => $id));
-	    	return $query->row_array();
+		function get_filename($id)
+		{
+		    	$query = $this->db->get_where('profile_pictures', array('account_no' => $id));
+		    	return $query->row_array();
 	  	}
 	}
 ?>
