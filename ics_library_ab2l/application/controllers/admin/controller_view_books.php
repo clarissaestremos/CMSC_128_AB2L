5afa484552fdbcae7906a73d7712f21b85a28e8e
annloisabendante <annloisabendante@gmail.com>
2014-03-18 17:40:12 +0800

    Update controller_view_books.php

--- ics_library_ab2l/application/controllers/admin/controller_view_books.php
+++ ics_library_ab2l/application/controllers/admin/controller_view_books.php
@@ -2,16 +2,17 @@
 include_once("controller_log.php");
 class Controller_view_books extends Controller_log {
  
+   /*Loads the list of books along with the data*/
     function index() {
     	$data['parent'] = "Books";
     	$data['current'] = "View Books";
         
-    		$this->load->helper(array('form','html'));
-	        $this->load->view("admin/view_header",$data);
-	        $this->load->view("admin/view_aside");
-	        $this->load->view("admin/view_books");
-	        $this->load->view("admin/view_footer");
-
+    	$this->load->helper(array('form','html'));
+        $this->load->view("admin/view_header",$data);
+        $this->load->view("admin/view_aside");
+        $this->load->view("admin/view_books");
+        $this->load->view("admin/view_footer");
+   }
 }
 /* End of file home_controller.php */
 /* Location: ./application/controllers/user/controller_home.php */
