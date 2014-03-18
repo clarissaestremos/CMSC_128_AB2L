5962672ea7d6b55aa6fdd0db62da21055dc4d97e
Klarence Kim <kimanangan@gmail.com>
2014-03-18 17:18:53 +0800

    Update model_get_list.php

--- ics_library_ab2l/application/models/model_get_list.php
+++ ics_library_ab2l/application/models/model_get_list.php
@@ -1,21 +1,25 @@
 <?php
-	class Model_get_list extends CI_Model{
+class Model_get_list extends CI_Model{
 
-	public function select_all_book_info($sort_by,$order_by,$data, $limit,$start){
-		if ($sort_by != "author" && $sort_by != "subject")
+	public function select_all_book_info($sort_by,$order_by,$data, $limit,$start)
+	{
+		if ($sort_by != "author" && $sort_by != "subject"){
 			$query=$this->db->order_by($sort_by,$order_by)->get('book');
-
-		else if ($sort_by == "author")
+		}
+		
+		else if ($sort_by == "author"){
 			$query = $this->db->query("SELECT DISTINCT b.*
 										FROM book b, book_author ba
 										WHERE b.id = ba.id
 										ORDER BY ba.author $order_by ");
+		}
 
-		else if ($sort_by == "subject")
+		else if ($sort_by == "subject"){
 			$query = $this->db->query("SELECT DISTINCT b.*
 										FROM book b, book_subject bs
 										WHERE b.id = bs.id
 										ORDER BY bs.subject $order_by");
+		}
 		
 
 		if($limit>0){	
@@ -47,65 +51,71 @@
 	}
 
 
-public function select_returned_books($account,$sort_by,$order_by,$data, $limit,$start){
-	
-	if ($sort_by == "due_date")
-			$query= $this->db->query("SELECT *
-			FROM book_reservation br, book b, user_account u, book_call_number cn
-			WHERE u.username='".$account."' 
-			AND br.call_number = cn.call_number
-			AND u.account_number = br.account_number
-			AND cn.id = b.id
-			AND br.status='returned'
-			GROUP BY br.call_number
-			ORDER BY br.due_date");
-
-	if($limit>0){	
-		if ($start == NULL)
-				$start = 0;
+	public function select_returned_books($account,$sort_by,$order_by,$data, $limit,$start)
+	{
 		if ($sort_by == "due_date")
-			$query= $this->db->query("SELECT * FROM book_reservation br, book b, user_account u, book_call_number cn
-					WHERE u.username='".$account."' 
-					AND br.call_number = cn.call_number
-					AND u.account_number = br.account_number
-					AND cn.id = b.id
-					AND br.status='returned'
-					GROUP BY br.call_number
-					ORDER BY br.due_date
-					LIMIT $start,$limit");
+				$query= $this->db->query("SELECT *
+				FROM book_reservation br, book b, user_account u, book_call_number cn
+				WHERE u.username='".$account."' 
+				AND br.call_number = cn.call_number
+				AND u.account_number = br.account_number
+				AND cn.id = b.id
+				AND br.status='returned'
+				GROUP BY br.call_number
+				ORDER BY br.due_date");
+	
+		if($limit>0){	
+			if ($start == NULL)
+					$start = 0;
+			if ($sort_by == "due_date")
+				$query= $this->db->query("SELECT * FROM book_reservation br, book b, user_account u, book_call_number cn
+						WHERE u.username='".$account."' 
+						AND br.call_number = cn.call_number
+						AND u.account_number = br.account_number
+						AND cn.id = b.id
+						AND br.status='returned'
+						GROUP BY br.call_number
+						ORDER BY br.due_date
+						LIMIT $start,$limit");
 		}
+		
 		return $query->result();
 }
 
-	public function get_book_call_numbers($id){
+	public function get_book_call_numbers($id)
+	{
 		$query = $this->db->get_where('book_call_number', array('id' => $id));
 		return $query->result();
 	}
 
-	public function get_book_authors($id){
+	public function get_book_authors($id)
+	{
 	 	$query = $this->db->get_where('book_author', array('id' => $id));
 		return $query->result();
 	}
 	
-	public function get_book_subjects($id){
+	public function get_book_subjects($id)
+	{
 		$query = $this->db->get_where('book_subject', array('id' => $id));
 		return $query->result();
 	}
 
-	public function get_book_tags($id){
+	public function get_book_tags($id)
+	{
 		$query = $this->db->get_where('tag', array('id' => $id));
 		return $query->result();
 	}
 	
-	public function get_edit_call_numbers($id){
+	public function get_edit_call_numbers($id)
+	{
 		$query = $this->db->query("SELECT call_number FROM book_call_number
 									WHERE id LIKE $id
 									AND call_number NOT IN
-										(SELECT call_number
-										FROM book_reservation
-										WHERE status NOT LIKE 'reserved'
-										OR status NOT LIKE 'overdue'
-										OR status NOT LIKE 'borrowed')");
+									(SELECT call_number
+									FROM book_reservation
+									WHERE status NOT LIKE 'reserved'
+									OR status NOT LIKE 'overdue'
+									OR status NOT LIKE 'borrowed')");
 		return $query->result();
 	}
 
@@ -113,22 +123,22 @@ public function select_returned_books($account,$sort_by,$order_by,$data, $limit,
 		$query = $this->db->query("SELECT call_number FROM book_call_number
 									WHERE id LIKE $id
 									AND call_number IN
-										(SELECT call_number
-										FROM book_reservation
-										WHERE status NOT LIKE 'reserved'
-										OR status NOT LIKE 'overdue'
-										OR status NOT LIKE 'borrowed')");
+									(SELECT call_number
+									FROM book_reservation
+									WHERE status NOT LIKE 'reserved'
+									OR status NOT LIKE 'overdue'
+									OR status NOT LIKE 'borrowed')");
 		return $query->result();
 	}
 
-	function get_book_by_id($id){
+	function get_book_by_id($id)
+	{
 		$query = $this->db->get_where('book', array('id' => $id));;
 		return $query->result();
 	}
 
-
-
-	function get_by_id($id){
+	function get_by_id($id)
+	{
 		$book = $this->db->get_where('book', array('id' => $id));;
 		$book_result = $book->result();
 
@@ -138,7 +148,8 @@ public function select_returned_books($account,$sort_by,$order_by,$data, $limit,
 		return $book_result;
 	}
 
-	public function get_authors($id){
+	public function get_authors($id)
+	{
 		$this->db->where('id', $id);
 		$authors = $this->db->get('book_author');
 		$authors = $authors->result();
@@ -153,7 +164,8 @@ public function select_returned_books($account,$sort_by,$order_by,$data, $limit,
 		return $author_array;
 	}
 
-	public function get_subjects($id){
+	public function get_subjects($id)
+	{
 		$this->db->where('id', $id);
 		$subjects = $this->db->get('book_subject');
 		$subjects = $subjects->result();
@@ -169,7 +181,8 @@ public function select_returned_books($account,$sort_by,$order_by,$data, $limit,
 	}
 
 
-	public function get_list($account,$status,$data,$limit,$start){
+	public function get_list($account,$status,$data,$limit,$start)
+	{
 		$query= $this->db->query("SELECT *
 		FROM book_reservation br, book b, user_account u, book_call_number cn
 		WHERE u.username='".$account."' 
@@ -180,21 +193,22 @@ public function select_returned_books($account,$sort_by,$order_by,$data, $limit,
 		GROUP BY br.call_number
 		ORDER BY br.due_date");
 
-		
-
 		return $query->result();
 	}
 
-	public function cancel_reservation($res_number){
+	public function cancel_reservation($res_number)
+	{
 		$this->db->where('res_number', $res_number);
 		$this->db->delete('book_reservation');
 	}
 
-	public function update_rank($call_number){
+	public function update_rank($call_number)
+	{
 		$this->db->query("UPDATE book_reservation SET  rank = rank-1 WHERE call_number = '".$call_number."' AND rank > 1 AND status = 'reserved' ");
 	}
 
-	public function update_available($call_number){
+	public function update_available($call_number)
+	{
 		$this->db->query("UPDATE book a,book_call_number c
 							SET a.no_of_available = a.no_of_available + 1
 							WHERE c.call_number = '".$call_number."' AND
