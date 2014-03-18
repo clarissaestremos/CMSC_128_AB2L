a69f39fad42b633b8bd818d816f163b16c53d371
odyzzadaleon <odyzzadaleon@gmail.com>
2014-03-18 18:19:15 +0800

    Added Documentation to controller_reserve_book.php

--- ics_library_ab2l/application/controllers/user/controller_reserve_book.php
+++ ics_library_ab2l/application/controllers/user/controller_reserve_book.php
@@ -8,12 +8,12 @@ class Controller_reserve_book extends CI_Controller{
 
 	function index(){
 		if($this->session->userdata('id') == FALSE){
-			redirect('index.php/user/controller_search_book', 'refresh');
+			redirect('index.php/user/controller_search_book', 'refresh');// redirect to controller_search_book
 		}
-		$data['titlepage'] = 'Reservation Page';
+		$data['titlepage'] = 'Reservation Page';  //displays reservation page
 		$data['id'] = urldecode($this->session->userdata('id'));
 		$row=$this->model_reserve_book->fetch_book($data['id']);
-		if($row->num_rows()>0){
+		if($row->num_rows()>0){ // displays book details from the database 
 			foreach($row->result() as $book_details){
 				$data['title'] = $book_details->title;
 				$data['year_of_pub'] = $book_details->year_of_pub;
@@ -30,55 +30,55 @@ class Controller_reserve_book extends CI_Controller{
 				$data['author'] = $newdata;	
 			}
 		}
-		$data['borrower'] = $this->session->userdata('logged_in')['username'];
-		$data['parent'] = "Books";
-    	$data['current'] = "Reserve";
+		$data['borrower'] = $this->session->userdata('logged_in')['username'];// the data borrower is the username of the logged-in user
+		$data['parent'] = "Books";  //the data parent will be "Books"
+    $data['current'] = "Reserve"; // the current data will be "Reserve"
 
         $this->load->helper(array('form','html'));
-        $this->load->view("user/view_header",$data);
-        $this->load->view("user/view_reserve_book", $data);
-        $this->load->view("user/view_footer");
+        $this->load->view("user/view_header",$data);  // displays the header
+        $this->load->view("user/view_reserve_book", $data); //  displays view_reserve_book
+        $this->load->view("user/view_footer");  //displays footer
 	}
 
-	function verify_login($id){
+	function verify_login($id){ // function for verification if the user is logged in
 		$id = urldecode($id);
 			$newdata = array(
 				'id' => $id
 				);
 			$this->session->set_userdata($newdata);
 			
-		if($this->session->userdata('logged_in') == FALSE){
+		if($this->session->userdata('logged_in') == FALSE){ // if the user is not logged in, redirect to login page
 			redirect('index.php/user/controller_login', 'refresh');
 		}
 		else{
-			redirect('index.php/user/controller_reserve_book');
+			redirect('index.php/user/controller_reserve_book'); // redirect to controller_reserve_book
 		}
 	}
 
-	function confirm_reservation(){
+	function confirm_reservation(){ // function to confirm reservation
 		$base = base_url();
-		if($this->session->userdata('id') != FALSE && $this->session->userdata('logged_in') != FALSE){
+		if($this->session->userdata('id') != FALSE && $this->session->userdata('logged_in') != FALSE){  // if user is logged in,
 			$data['id'] = $this->session->userdata('id');
 			$this->session->unset_userdata('id');
 			$row = $this->model_reserve_book->fetch_user($this->session->userdata('logged_in')['username']);
-			foreach ($row->result() as $value) {
+			foreach ($row->result() as $value) {  // an account number will be assigned to the borrower
 				$data['borrower'] = $value->account_number;
 			}
-			$num_borrowed = $this->model_reserve_book->fetch_user_reservation($data['borrower'])->num_rows();
-				if($num_borrowed < 3){
+			$num_borrowed = $this->model_reserve_book->fetch_user_reservation($data['borrower'])->num_rows(); // get the number of borrowed books
+				if($num_borrowed < 3){  //if it is less than 3, allow user to reserve book
 				$row = $this->model_reserve_book->fetch_user2($data['borrower']);
 				foreach ($row->result() as $value) {
-					$user_status = $value->status;
+					$user_status = $value->status;  //a status will be assigned as user_status
 				}
-				if($user_status == 'approve'){
+				if($user_status == 'approve'){  //user_staus is approve
 					$row = $this->model_reserve_book->fetch_book($data['id']);
 					if($row->num_rows() == 1){
 						foreach ($row->result() as $value) {
-							$no_of_available = $value->no_of_available;
+							$no_of_available = $value->no_of_available; //number of available books will be assigned to $no_of_available
 						}
 					}
 					
-					if($no_of_available > 0){
+					if($no_of_available > 0){ //if available book is greater than 0, prompt success in reserving the book
 						if($this->model_reserve_book->add_reservation($data)){
 							echo "<div id='mysuccess' title='Success: Reserved'>
 									<h5>You have successfully reserved a book. Please confirm it to the administrator..</h5>
@@ -111,6 +111,7 @@ class Controller_reserve_book extends CI_Controller{
 						
 					}
 					else{
+					  //else, prompt that user is waitlisted for reservation of book
 						if($this->model_reserve_book->waitlist_reservation($data)){
 						echo "<div id='mysuccess' title='Success: Waitlisted'>
 									<h5>There is not enough number of books available. You are waitlisted.</h5>
@@ -145,6 +146,7 @@ class Controller_reserve_book extends CI_Controller{
 					}	
 				}
 				else{
+				  // else, the account is not yet validated by the admin
 					echo "<div id='mysuccess' title='Error: Account Activation'>
 									<h5>Your account is not yet activated. Please confirm it to the administrator.</h5>
 								</div>
@@ -176,6 +178,7 @@ class Controller_reserve_book extends CI_Controller{
 				
 			}
 			else{
+			  //else, prompt that the user already exceeded the limits of reserving a book
 				echo "<div id='mysuccess' title='Error: Reservation Limitation'>
 									<h5>A user is allowed to borrow and reserve at most 3 books</h5>
 								</div>
@@ -207,9 +210,9 @@ class Controller_reserve_book extends CI_Controller{
 			
 		}
 		else{
-			if($this->session->userdata('logged_in'))
+			if($this->session->userdata('logged_in')) // if logged in, redirect to controller_search_book
 				redirect('index.php/user/controller_search_book', 'refresh');
-			else redirect('index.php/user/controller_login', 'refresh');
+			else redirect('index.php/user/controller_login', 'refresh');// else, redirect to login page
 			
 		}
 		
