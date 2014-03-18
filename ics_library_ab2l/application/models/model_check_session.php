18097f63623bb0995fbaf11715ba6f3fbbf3f86b
Klarence Kim <kimanangan@gmail.com>
2014-03-18 17:15:20 +0800

    Update model_check_session.php

--- ics_library_ab2l/application/models/model_check_session.php
+++ ics_library_ab2l/application/models/model_check_session.php
@@ -1,16 +1,19 @@
 <?php
 class Model_check_session extends CI_Model {
 
-	public function check_session(){
+	public function check_session()
+	{
 		//if user, return true
 		if($this->session->userdata('logged_in_type')=="user"){
          	return true;
         }
+        
         //else if, admin, redirect to admin page
 		else if($this->session->userdata('logged_in_type')=="admin"){
 
 			redirect('index.php/admin/controller_admin_home');
 		}
+		
 		//else, return false
 		else{
 			redirect('index.php/user/controller_login');
@@ -18,11 +21,13 @@ class Model_check_session extends CI_Model {
 		}
 	}
 
-	public function check_admin_session(){
+	public function check_admin_session()
+	{
 		//if admin, return true
 		if($this->session->userdata('logged_in_type')=="admin"){
          	return true;
         }
+        
         //else if, user, redirect to user home
 		else if($this->session->userdata('logged_in_type')=="user"){
 
