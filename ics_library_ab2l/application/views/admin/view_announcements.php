d762e33b8652386efbd179d3eabcdbfaeb71dce2
annemurielg <annemurielg@gmail.com>
2014-03-18 18:18:22 +0800

    Update view_announcements.php

--- ics_library_ab2l/application/views/admin/view_announcements.php
+++ ics_library_ab2l/application/views/admin/view_announcements.php
@@ -14,51 +14,57 @@ $counter = 0;
 $txt_file = file_get_contents('./application/announcements.txt');
 $rows = explode("*", $txt_file);
 array_shift($rows);
-if($rows != NULL){
-foreach($rows as $row => $data)
+if($rows != NULL)
 {
-	$counter = $counter + 1;
-	$data1 = explode("^",$data);
-	$info[$row]['date'] = $data1[0]; 
-	//$info[$row]['tc'] = $data1[1];
 
-	if($counter>5) break;
-	//echo 'Date: ' . ($date=$info[$row]['date']) . '<br />';
-
-	array_shift($data1);
-	$count = 0;
-	foreach($data1 as $row1 => $data2)
+	foreach($rows as $row => $data)
 	{
-		$row_data = explode('#', $data2);
-		$info[$row1]['title'] = $row_data[0];
-		$info[$row1]['content'] = $row_data[1];
+		$counter = $counter + 1;
+		$data1 = explode("^",$data);
+		$info[$row]['date'] = $data1[0]; 
+		//$info[$row]['tc'] = $data1[1];
 
-		echo "<div class='panel cell'>";
-		echo "<div class='gradient header'>Title: {$info[$row1]['title']}
-		<form action='controller_announcement/find' class='float-right' method='post'>
+		if($counter>5) break;
+		//echo 'Date: ' . ($date=$info[$row]['date']) . '<br />';
+
+		array_shift($data1);
+		$count = 0;
+
+		foreach($data1 as $row1 => $data2)
+		{
+			$row_data = explode('#', $data2);
+			$info[$row1]['title'] = $row_data[0];
+			$info[$row1]['content'] = $row_data[1];
+
+			echo "<div class='panel cell'>";
+			echo "<div class='gradient header'>Title: {$info[$row1]['title']}
+			<form action='controller_announcement/find' class='float-right' method='post'>
 				<input type='hidden' name='date' ' value='{$info[$row]['date']}' />
 				<input type='hidden' name='delete'/>
 				<input type='submit' name='edit' style='height:1.5em; font-size: 10px; line-height: 0px;' value='Edit' enabled/>
 				<input type='submit' value='Delete' id='delete$count' style='height:1.5em; font-size:10px; line-height: 0px;' enabled/>
 				</form><br/>
-		Date: {$info[$row]['date']}
+			Date: {$info[$row]['date']}
 				</div>";
-		echo "<div class='body'>";
-		echo "<div class='cell'>";
-		echo "<div>";
-		echo "</div>";
-		echo "{$info[$row1]['content']}<br/>";
-		echo "</div>";
-		echo "</div>";
-		echo "</div><hr/>";
-		$count++;
-	}
+			echo "<div class='body'>";
+			echo "<div class='cell'>";
+			echo "<div>";
+			echo "</div>";
+			echo "{$info[$row1]['content']}<br/>";
+			echo "</div>";
+			echo "</div>";
+			echo "</div><hr/>";
+			$count++;
+		}
 	
+	}
 }
-}
-else{
+
+else
+{
 	echo "<div class='cell'><h2>There is no announcement to display!</h2></div><hr/>";
 }
+
 	echo "<form action='controller_announcement/deleteAll' id='deleteall' class='float-right' style='margin-left: 5px;' method='post'>
 			<input type='hidden' name='delete_all'/>
 			<input type='submit' value='Delete All Announcements' enabled/>
@@ -69,18 +75,23 @@ else{
 
 		";
 ?>
+
 <div id="deletealldialog" title="Delete All Announcement Confirmation">
 	<p>Are you sure that you want to delete all the announcements?</o>
 </div>
+
 <div id="deletedialog" title="Delete Announcement Confirmation">
 	<p>Are you sure that you want to delete this announcement?</o>
 </div>
+
 <div id="deleteconfirm" title="Delete All Announcement Confirmation">
 	<p>Are you really sure that you want to delete all announcements? Doing so will remove it from the database.</p>
 </div>
+
 <div id="dsucc" title="Delete Announcement Success">
    <p>You have successfuly deleted an announcement!</p>
 </div>
+
 <div id="dasucc" title="Delete All Announcement Success">
   <p>You have successfully deleted all the announcements!</p>
 </div>
