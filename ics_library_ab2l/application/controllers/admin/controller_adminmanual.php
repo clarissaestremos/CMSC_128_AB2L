b350dd148fbeb85ab60592ab1e586eff661f6594
arriane1111 <krystelarrianecortez@gmail.com>
2014-03-18 18:38:55 +0800

    Update controller_adminmanual.php

--- ics_library_ab2l/application/controllers/admin/controller_adminmanual.php
+++ ics_library_ab2l/application/controllers/admin/controller_adminmanual.php
@@ -1,11 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Controller_adminmanual extends CI_Controller {
- 
+  /**
+   * CAlls for the view of the adminmanual page
+   * */
     function index() {
         $this->load->helper(array('form','html'));
         $this->load->view("admin/view_adminmanual");
         
     }
 }
-/* End of file home_controller.php */
-/* Location: ./application/controllers/user/controller_home.php */
+/* End of file controller_adminmanual.php */
+/* Location: ./application/controllers/admin/controller_adminmanual.php */
