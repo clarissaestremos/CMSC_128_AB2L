a797a502876db0ab4e6a8b32c80d2a8307881efb
odyzzadaleon <odyzzadaleon@gmail.com>
2014-03-18 17:19:53 +0800

    Added Documentation

--- ics_library_ab2l/application/controllers/user/controller_login.php
+++ ics_library_ab2l/application/controllers/user/controller_login.php
@@ -1,34 +1,32 @@
 
-
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Controller_login extends CI_Controller {
-	//http://imron02.wordpress.com/2013/06/01/simple-login-using-codeigniter-database/
-	function __construct() {
-        parent::__construct();
-        $this->load->helper('url');
-        $this->load->library(array('form_validation','session'));
+  class Controller_login extends CI_Controller {
+  //http://imron02.wordpress.com/2013/06/01/simple-login-using-codeigniter-database/
+    function __construct() {
+      parent::__construct();
+      $this->load->helper('url');
+      $this->load->library(array('form_validation','session'));
     }
-
-
+    
+  
     function index(){
-
-        $this->login();
+      $this->login();
     }
+    
     function login() {
-    	if($this->session->userdata('logged_in')){
-    		redirect(base_url(), 'refresh');
-    	}else{
-	        $this->load->helper(array('form','html'));
-	        $data['user'] = $this->session->userdata('logged_in');
-           
-            $data['titlepage']= "Login Page";
-            $this->load->view("user/view_header", $data);
-            $this->load->view('user/view_login'); //load view for login
-            $this->load->view('user/view_footer');
-    	}
+      if($this->session->userdata('logged_in')){
+        redirect(base_url(), 'refresh');
+      }
+      else{
+        $this->load->helper(array('form','html'));
+        $data['user'] = $this->session->userdata('logged_in');
         
-       
+        $data['titlepage']= "Login Page";
+        $this->load->view("user/view_header", $data); //display header
+        $this->load->view('user/view_login'); //load view for login
+        $this->load->view('user/view_footer');  //display footer
+      }
     }
-}
+  }
 /* End of file login_controller.php */
 /* Location: ./application/controllers/user/controller_login.php */
