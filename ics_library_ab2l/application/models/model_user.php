50d8fb462975ec48028d8114270dfc5073457bb8
Klarence Kim <kimanangan@gmail.com>
2014-03-18 18:08:07 +0800

    Update model_user.php

--- ics_library_ab2l/application/models/model_user.php
+++ ics_library_ab2l/application/models/model_user.php
@@ -25,26 +25,30 @@ class Model_user extends CI_Model {
 		return $query->result();
 	}
 	
-	public function remove_pending(){
+	public function remove_pending()
+	{
 		//automatic removal of 1 month pending requests
 		$query = $this->db->query("DELETE 
 			FROM user_account
 			WHERE (status = 'pending') AND (datediff(curdate(), date_notif) >= 30)");
 	}
 
-	public function approve_user($account_number){
+	public function approve_user($account_number)
+	{
 		$query = $this->db->query("UPDATE user_account 
 			SET status='approved', date_notif=curdate() 
 			WHERE account_number='{$account_number}'");
 	}
 
-	public function remove_user($account_number){
+	public function remove_user($account_number)
+	{
 		$query = $this->db->query("DELETE 
 			FROM user_account
 			WHERE account_number='{$account_number}'");
 	}
 	
-	public function deactivate_users(){
+	public function deactivate_users()
+	{
 		$this->db->select('account_number');
 		$query = $this->db->get('user_account');
 		foreach($query->result() as $user){
